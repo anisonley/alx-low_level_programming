2e763da4237b2249586fabe689fc6b70cd3a71ba
anisonley <anisonley@gmail.com>
2023-02-16 11:33:46 -0800

    Task 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - A program that prints a line with the funtion puts and not printf.
+ * return: 0
+ */
+
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
