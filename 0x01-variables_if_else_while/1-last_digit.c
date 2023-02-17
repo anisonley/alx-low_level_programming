346df60bce4b24cfd7f6f9aadb9e2e7dbc245a01
anisonley <anisonley@gmail.com>
2023-02-17 08:13:54 -0800

    later commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - 
+ * main - This program will assign a random number to the variable.
  * Return: 0 (success)
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 		if ((n % 10) > 5)
