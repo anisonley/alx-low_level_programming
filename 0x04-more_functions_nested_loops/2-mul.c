3232674f0f9dc4d9b3c8e3710444ccc09c8a4a26
anisonley <anisonley@gmail.com>
2023-02-23 07:27:42 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - checks for checks for a digit (0 through 9).
+ * @a: a - first in
+ * @b: b - second int
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ReadMe for 0x04. C - More functions, more nested loops
