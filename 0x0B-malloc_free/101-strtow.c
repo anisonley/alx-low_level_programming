ba6a687312334318af1bf729ff83977d2ff3148c
anisonley <anisonley@gmail.com>
2023-03-14 11:41:54 -0700

    Love

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -44,6 +44,33 @@ flag = 1;
  */
 char **strtow(char *str)
 {
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
 
@@ -69,7 +96,7 @@ char **strtow(char *str)
 					return (NULL);
 
 				while (start < end)
-					*tmp++ = str[start++];
+				*tmp++ = str[start++];
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
@@ -79,6 +106,9 @@ char **strtow(char *str)
 		else if (c++ == 0)
 			start = i;
 	}
+
 	matrix[k] = NULL;
+
 	return (matrix);
 }
+
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	if (mee == NULL)
 		return (NULL);
 
-	for (x = 0; x < height; x++)		
+	for (x = 0; x < height; x++)
 	{
 		mee[x] = malloc(sizeof(int) * width);
 
